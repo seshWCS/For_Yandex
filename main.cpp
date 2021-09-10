74c7dcad84ba696a089661f34f90180b3d239eb9
Александр Нам <aleksandrnam@MacBook-Air-Aleksandr.local>
2021-09-10 12:08:29 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstddef>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <ctime>
+#include <unordered_map>
+#include <random>
+#include <iomanip>
+#include <cmath>
+#include <queue>
+#include <unordered_set>
+#include <cassert>
+#include <bitset>
+#include <deque>
+#include <utility>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("vpt")
+
+
+using namespace std;
+
+#define ld long double
+#define vi vector<int>
+#define pii pair<int, int>
+//#define int long long
+
+const int INF = 2e9;
+
+const int mod = 998244353;
+
+void solve() {
+
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
